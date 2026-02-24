f90b4d50a491f2991797e08dfc413af87bab2b6e
Bohdan Lytvynenko <b.lytvynenko.off@gmail.com>
2026-02-24 18:43:30 +0200

    Spring Beans Deep Dive — AutoWiring, Scopes and More - Exercise 3

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -10,18 +10,10 @@ public class Main
 {
 	static void main()
 	{
-
 		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-		var veh = context.getBean("audiVehicle", Vehicle.class);
+		var veh = context.getBean(Vehicle.class);
 		System.out.println("Vehicle name from Spring Context is: " + veh.getName());
-
-		var vehicle = (Vehicle) context.getBean("myFavouriteVehicle");
-		System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
-
-		var vhcle = context.getBean(Vehicle.class);
-		System.out.println("Vehicle name from Spring Context is: " + vhcle.getName());
-
-		var helloWorld = context.getBean(String.class);
-		System.out.println("String value from Spring Context is: " + helloWorld);
+		veh.sayHello();
+		context.close();
 	}
 }
--- src/main/java/com/demo/config/ProjectConfig.java
+++ src/main/java/com/demo/config/ProjectConfig.java
@@ -1,12 +1,7 @@
 package com.demo.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-
-import com.demo.beans.Vehicle;
 
 
 /*
@@ -16,34 +11,8 @@ methods. So Spring container can process the class and generate Spring Beans
 to be used in the application.
  */
 @Configuration
-@Import({ AnotherProjectConfig.class })
+@ComponentScan(basePackages = { "com.demo.beans" })
 public class ProjectConfig
 {
-
-	@Bean(name = "audiVehicle")
-	Vehicle vehicle1()
-	{
-		var veh = new Vehicle();
-		veh.setName("Audi");
-		return veh;
-	}
-
-	@Primary
-	@Bean(value = "hondaVehicle")
-	Vehicle vehicle2()
-	{
-		var veh = new Vehicle();
-		veh.setName("Honda");
-		return veh;
-	}
-
-	@Bean({ "ferrariVehicle", "myFavouriteVehicle" })
-	@Description("This is a Vehicle class bean")
-	Vehicle vehicle3()
-	{
-		var veh = new Vehicle();
-		veh.setName("Ferrari");
-		return veh;
-	}
 
 }
