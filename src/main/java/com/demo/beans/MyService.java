90778ad775e6f10dbad61d1b60cc0c3b0dcbab97
Bohdan Lytvynenko <31684405+Bohdan-Lytvynenko-dev@users.noreply.github.com>
2026-02-25 23:12:59 +0200

    Section 2 - Spring Beans Deep Dive - AutoWiring, Scopes and More (#3)
    Spring Beans Deep Dive — AutoWiring, Scopes and More

---
+++ src/main/java/com/demo/beans/MyService.java
@@ -0,0 +1,20 @@
+package com.demo.beans;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Scope(BeanDefinition.SCOPE_SINGLETON) // Optional
+@Lazy
+public class MyService
+{
+
+	public MyService()
+	{
+		System.out.println("MyService Bean is created");
+	}
+
+}
